dc039b9d97d33f6618e6ce4e2b784512e7d669bb
Neha Bandal <nehabandal@Nehas-MacBook-Pro.local>
2018-07-18 18:08:39 -0700

    Updated config.js file

--- Project-UnicornRides/aws-serverless-workshops-master/WebApplication/1_StaticWebHosting/website/js/config.js
+++ Project-UnicornRides/aws-serverless-workshops-master/WebApplication/1_StaticWebHosting/website/js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_69IUNixKX', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'bbn6ifk5qvh8bplo0i8k6e8dh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
